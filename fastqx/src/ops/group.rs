548d060fa9442cf4a1469eb3405366fa42bfc70a
jacob <jacobbishopxy@gmail.com>
2023-10-08 00:47:23 +0800

    :rocket: simplify `group`

--- fastqx/src/ops/group.rs
+++ fastqx/src/ops/group.rs
@@ -9,21 +9,21 @@ use itertools::Itertools;
 use ref_cast::RefCast;
 
 use crate::adt::*;
-use crate::ops::{FqxRowSelect, FqxSlice};
 
 // ================================================================================================
 // OpGroup
 // ================================================================================================
 
-pub trait OpGroup<K, I>
+pub trait OpGroup<K, T>
 where
     K: PartialEq,
 {
+    type Item;
     type Ret<A>;
 
-    fn group_by<F>(self, f: F) -> Self::Ret<I>
+    fn group_by<F>(self, f: F) -> Self::Ret<Self::Item>
     where
-        F: Fn(&I) -> K;
+        F: Fn(&Self::Item) -> K;
 }
 
 // ================================================================================================
@@ -38,96 +38,48 @@ pub struct FqxGroup<A>(pub(crate) HashMap<Vec<FqxValue>, A>);
 // Impl
 // ================================================================================================
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// FqxData
-
-impl OpGroup<Vec<FqxValue>, FqxRow> for FqxData {
-    type Ret<A> = FqxGroup<Vec<A>>;
-
-    fn group_by<F>(self, f: F) -> Self::Ret<FqxRow>
-    where
-        F: Fn(&FqxRow) -> Vec<FqxValue>,
-    {
-        let mut res = HashMap::new();
-        self.iter_owned()
-            .group_by(f)
-            .into_iter()
-            .for_each(|(k, g)| res.entry(k).or_insert(Vec::new()).extend(g.collect_vec()));
-
-        FqxGroup(res)
-    }
-}
-
-impl<'a> OpGroup<Vec<FqxValue>, &'a FqxRow> for &'a FqxData {
-    type Ret<A> = FqxGroup<Vec<A>>;
-
-    fn group_by<F>(self, f: F) -> Self::Ret<&'a FqxRow>
-    where
-        F: Fn(&&'a FqxRow) -> Vec<FqxValue>,
-    {
-        let mut res = HashMap::new();
-        self.iter()
-            .group_by(f)
-            .into_iter()
-            .for_each(|(k, g)| res.entry(k).or_insert(Vec::new()).extend(g.collect_vec()));
-
-        FqxGroup(res)
-    }
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// FqxSlice
-
-impl<'a> OpGroup<Vec<FqxValue>, &'a FqxRow> for &'a FqxSlice {
-    type Ret<A> = FqxGroup<Vec<A>>;
-
-    fn group_by<F>(self, f: F) -> Self::Ret<&'a FqxRow>
-    where
-        F: Fn(&&'a FqxRow) -> Vec<FqxValue>,
-    {
-        let mut res = HashMap::new();
-        self.0
-            .iter()
-            .group_by(f)
-            .into_iter()
-            .for_each(|(k, g)| res.entry(k).or_insert(Vec::new()).extend(g.collect_vec()));
-
-        FqxGroup(res)
-    }
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// FqxSelect
-
-impl OpGroup<Vec<FqxValue>, FqxRowSelect<FqxValue>> for Vec<FqxRowSelect<FqxValue>> {
-    type Ret<A> = FqxGroup<Vec<A>>;
-
-    fn group_by<F>(self, f: F) -> Self::Ret<FqxRowSelect<FqxValue>>
-    where
-        F: Fn(&FqxRowSelect<FqxValue>) -> Vec<FqxValue>,
-    {
-        let mut res = HashMap::new();
-        self.into_iter()
-            .group_by(f)
-            .into_iter()
-            .for_each(|(k, g)| res.entry(k).or_insert(Vec::new()).extend(g.collect_vec()));
-
-        FqxGroup(res)
-    }
-}
-
-impl<'a> OpGroup<Vec<FqxValue>, &'a FqxRowSelect<&'a FqxValue>>
-    for &'a Vec<FqxRowSelect<&'a FqxValue>>
+impl<I, V, T, E> OpGroup<Vec<FqxValue>, FqxRowAbstract<I, V>> for T
+where
+    I: IntoIterator<Item = V>,
+    V: Into<FqxValue>,
+    T: IntoIterator<Item = E>,
+    E: Into<FqxRowAbstract<I, V>>,
 {
+    type Item = E;
+
     type Ret<A> = FqxGroup<Vec<A>>;
 
-    fn group_by<F>(self, f: F) -> Self::Ret<&'a FqxRowSelect<&'a FqxValue>>
+    fn group_by<F>(self, f: F) -> Self::Ret<Self::Item>
     where
-        F: Fn(&&'a FqxRowSelect<&'a FqxValue>) -> Vec<FqxValue>,
+        F: Fn(&Self::Item) -> Vec<FqxValue>,
     {
         let mut res = HashMap::new();
-        self.iter()
-            .group_by(f)
+        Itertools::group_by(self.into_iter(), f)
+            .into_iter()
+            .for_each(|(k, g)| res.entry(k).or_insert(Vec::new()).extend(g.collect_vec()));
+
+        FqxGroup(res)
+    }
+}
+
+impl<'a, I, V, T, E> OpGroup<Vec<FqxValue>, &'a FqxRowAbstract<I, V>> for &'a T
+where
+    I: IntoIterator<Item = V> + 'a,
+    V: Into<FqxValue> + 'a,
+    T: ?Sized,
+    for<'b> &'b T: IntoIterator<Item = &'b E>,
+    E: AsRef<FqxRowAbstract<I, V>> + 'a,
+{
+    type Item = &'a E;
+
+    type Ret<A> = FqxGroup<Vec<A>>;
+
+    fn group_by<F>(self, f: F) -> Self::Ret<Self::Item>
+    where
+        F: Fn(&Self::Item) -> Vec<FqxValue>,
+    {
+        let mut res = HashMap::new();
+        Itertools::group_by(self.into_iter(), f)
             .into_iter()
             .for_each(|(k, g)| res.entry(k).or_insert(Vec::new()).extend(g.collect_vec()));
 
